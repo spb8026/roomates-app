1d7e9e2ccdb9e9673c18cf5d4e0e0127d12cf26c
Shawn Broderick <spb8026@rit.edu>
2024-09-16 16:44:38 -0400

    Setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <link rel="manifest" href="/maifest.json" />
       <body>
         <Main />
         <NextScript />
